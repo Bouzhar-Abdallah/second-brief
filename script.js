2955bc6e1bb7a46f8b79be088e1ab043d7a620b2
Abdallah Bouzhar <abddoullah.bouzhar@gmail.com>
2022-10-28 01:16:23 +0100

    menu for mobile good

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("mini-menu").addEventListener("click", 
+showMobileMenu
+);
+function showMobileMenu () {
+    document.getElementById("titles-mobile").style.display = "block";
+
+
+}
