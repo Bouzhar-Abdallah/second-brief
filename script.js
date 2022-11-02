5536ded9961f954982b5b497c6145afa24d96791
abdallah bouzhar <abddoullah.bouzhar@gmail.com>
2022-11-02 09:22:29 +0100

    contact

--- script.js
+++ script.js
@@ -7,3 +7,69 @@ function showMobileMenu () {
 
 
 }
+
+
+/* jjjjjjjj */
+
+document.getElementById("mini-menu").addEventListener("click", 
+showMobileMenu
+);
+function showMobileMenu () {
+    document.getElementById("titles-mobile").style.display = "block";
+
+
+}
+
+
+const openContainerButtons=document.getElementById("open-form")
+
+openContainerButtons.addEventListener("click",  writeHtml)
+
+const bg=document.getElementById('bg')
+
+
+
+
+function writeHtml()
+    
+{
+  document.body.innerHTML += 
+  ` 
+  <link rel="stylesheet" href="./style-form.css">
+  <div class="container form"  id="container">
+  
+      <div class="containerbg"></div>
+      <div class="horizontal">
+          <figure><img src="./logo/Unsdvsvtitled-1.png" alt="" srcset=""></figure>
+          <div class="vertical">
+                  <div>
+                  <label for="" >Full Name :</label>
+                  <input type="text">
+              </div>
+              <div>
+                  <label for="" >E-mail :</label>
+                  <input type="text">
+              </div>
+              <div>
+                  <label for="" >Message :</label>
+                  <textarea name="" id="" cols="30" rows="10"></textarea>
+              </div>
+          </div>
+
+      </div>
+      <div class="footer">
+      <button  id="delete-form" onclick="container.remove();">Send</button>
+      </div>
+      <div class="bg" id="bg"></div>
+      </div>
+      
+  </div>`;
+
+};
+
+function deleteHtml(container)
+    
+{
+    
+ container.remove();
+};
